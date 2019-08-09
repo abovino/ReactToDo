2c7a71b300e07935bbd2e811bfc6192406ea5593
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-08 21:52:32 -0400

    App component for test

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
+
+const App = () => (
+  <h1 className='App'>Hey</h1>
+)
+
+export default hot(module)(App);
