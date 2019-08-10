5ff0b8811c082eb2908604250e9887eddcecf302
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-10 19:05:37 -0400

    Move main App component to components dir

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-import './App.css';
-
-const App = () => (
-  <h1 className='App'>Hey</h1>
-)
-
-export default hot(module)(App);
