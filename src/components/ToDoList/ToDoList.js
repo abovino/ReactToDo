03d84d6d9a28a16ddd95d755052dbcc2b16c31e4
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-11 17:43:21 -0400

    Add .list-container to wrap lists

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -5,7 +5,7 @@ import ToDosContext from '../../context/todos-context';
 const ToDoList = (props) => {
 	const { toDos } = useContext(ToDosContext);
 	return (
-		<div>
+		<div className='list-container'>
 			{toDos.map((toDo, i) => <ToDo key={toDo.id} toDo={toDo} />)}
 		</div>
 	)
