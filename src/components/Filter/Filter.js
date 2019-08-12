3e4b2f39c2725586d63661b4c73b8e617b077f07
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-11 20:22:17 -0400

    Passed callback for setting filter state

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import ToDosContext from '../../context/todos-context';
 
-const Filter = () => {
+const Filter = ({ setFilter }) => {
 	const { toDos } = useContext(ToDosContext);
 	return (
 		<div className='footer'>
-			<button className='filter-btn'>All</button>
-			<button className='filter-btn'>Active</button>
-			<button className='filter-btn'>Complete</button>
+			<button className='filter-btn' onClick={() => setFilter(undefined)} >All</button>
+			<button className='filter-btn' onClick={() => setFilter(true)} >Active</button>
+			<button className='filter-btn' onClick={() => setFilter(false)} >Complete</button>
 		</div>
 	)
 }
