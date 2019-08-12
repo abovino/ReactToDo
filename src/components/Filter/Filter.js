14b9a47a418ed686eba017f325a49d786cf1a858
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-12 18:51:46 -0400

    Added conditional filter-btn-active class

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import ToDosContext from '../../context/todos-context';
 
-const Filter = ({ setFilter }) => {
+const Filter = ({ setFilter, filter }) => {
 	const { toDos } = useContext(ToDosContext);
 	return (
 		<div className='footer'>
-			<button className='filter-btn' onClick={() => setFilter(undefined)} >All</button>
-			<button className='filter-btn' onClick={() => setFilter(true)} >Active</button>
-			<button className='filter-btn' onClick={() => setFilter(false)} >Complete</button>
+			<button className={filter === undefined ? 'filter-btn filter-btn-active':'filter-btn'} onClick={() => setFilter(undefined)} >All</button>
+			<button className={filter === true ? 'filter-btn filter-btn-active':'filter-btn'} onClick={() => setFilter(true)} >Active</button>
+			<button className={filter === false ? 'filter-btn filter-btn-active':'filter-btn'} onClick={() => setFilter(false)} >Complete</button>
 		</div>
 	)
 }
