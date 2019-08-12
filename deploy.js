f8411f9e81c1817e494516e5917a529582dcfdad
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-12 19:46:07 -0400

    Script to send build file to SFTP server

---
+++ deploy.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const Client = require('ssh2-sftp-client');
+const sftp = new Client();
+const localDistPath = './public/dist/bundle.js';
+const remoteDistPath = process.env.REMOTE_DIST_PATH;
+sftp.connect({
+  host: process.env.SFTP_HOST,
+  username: process.env.SFTP_USER,
+  password: process.env.SFTP_PASSWORD,
+}).then(() => {
+  return sftp.put(localDistPath, remoteDistPath)
+}).then(res => {
+  console.log(res);
+}).then(() => {
+  return sftp.end();
+}).catch(err => {
+  console.log(err);
+});
