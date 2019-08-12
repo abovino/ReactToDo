eb55e19d523b01874920788a6a4b7eaeb153b0d0
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-11 20:22:50 -0400

    Get the filter prop

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -3,7 +3,7 @@ import ToDo from './ToDo/ToDo';
 import ToDosContext from '../../context/todos-context';
 
 const ToDoList = (props) => {
-	const { toDos } = useContext(ToDosContext);
+	const { toDos, filter } = useContext(ToDosContext);
 	return (
 		<div className='list-container'>
 			{toDos.map((toDo, i) => <ToDo key={toDo.id} toDo={toDo} />)}
