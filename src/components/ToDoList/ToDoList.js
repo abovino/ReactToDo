464ec3598c6992184040d6b34d8ec4d81cf73313
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-12 20:57:31 -0400

    Add transition group for todo fade in and out

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -2,17 +2,25 @@ import React, { useContext } from 'react';
 import ToDo from './ToDo/ToDo';
 import ToDosContext from '../../context/todos-context';
 
+import { CSSTransitionGroup } from 'react-transition-group';
+
 const ToDoList = (props) => {
-	const { toDos, filter } = useContext(ToDosContext);
+  const { toDos, filter } = useContext(ToDosContext);
+  const items = toDos.map((toDo, i) => {
+    if (toDo.isComplete !== filter) {
+      return <ToDo key={toDo.id} toDo={toDo} />
+    } else if (filter === undefined) {
+      return <ToDo key={toDo.id} toDo={toDo} />
+    }
+  })
 	return (
 		<div>
-			{toDos.map((toDo, i) => {
-        if (toDo.isComplete !== filter) {
-          return <ToDo key={toDo.id} toDo={toDo} />
-        } else if (filter === undefined) {
-          return <ToDo key={toDo.id} toDo={toDo} />
-        }
-      })}
+      <CSSTransitionGroup
+        transitionName="fade-todo"
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={300}>
+        {items}
+      </CSSTransitionGroup>
 		</div>
 	)
 }
